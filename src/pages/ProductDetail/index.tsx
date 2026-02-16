e28a8d7fa54eb38e73d367e0a1d5bb07694cc6ae
Elgun Heydarli <elgunheyderli2016@gmail.com>
2026-02-16 14:19:38 +0400

    Məhsul səhifəsindən FAQ bölməsi silindi

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,38 +1,12 @@
-import ProductFAQ from '@/components/ProductDetail/ProductFAQ';
 import ProductMainDetail from '@/components/ProductDetail/ProductMainDetail';
-import { useGetProductDetail } from '@/services/products';
 import { SEO } from '@/components/SEO';
-import { useParams } from 'react-router';
 
 const ProductDetail = () => {
-  const { id } = useParams<{ id: string }>();
-
-  const { data: productDetailData } = useGetProductDetail(id ?? '');
-
-  const productDetail = productDetailData?.data;
-
-  const faqTitles = productDetail?.titles ?? [];
-
   return (
     <>
       <SEO seoKey="productDetail" />
-      {/* <BreadCrumbTitle
-        breadcrumbs={[
-          {
-            title: translateds('home_page'),
-            link: '/',
-          },
-          {
-            title: translateds('store'),
-          },
-          {
-            title: productDetail?.name,
-          },
-        ]}
-      /> */}
       <div className="pt-[118px]">
         <ProductMainDetail />
-        <ProductFAQ titles={faqTitles} />
       </div>
     </>
   );
